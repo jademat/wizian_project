7c3b4e333fe54c2c050b6c17aadb7b6ed230af80
jademat <01088049128a@gmail.com>
2025-04-05 12:46:04 +0900

    Initial commit

---
+++ src/main/java/com/hlb/wizian_project/WizianProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hlb.wizian_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizianProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizianProjectApplication.class, args);
+    }
+
+}
