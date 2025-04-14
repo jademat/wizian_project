d3f7032cdc60a32601e988481b48099408ffbdb7
41817 <bangjungbin@gmail.com>
2025-04-14 17:40:47 +0900

    archive 페이지 백엔드 기본 클래스 생성

---
+++ src/main/java/com/hlb/wizian_project/instructors/controller/ArchiveController.java
@@ -0,0 +1,35 @@
+package com.hlb.wizian_project.instructors.controller;
+
+import com.hlb.wizian_project.domain.*;
+import com.hlb.wizian_project.instructors.service.ArchiveService;
+import com.hlb.wizian_project.instructors.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins= {"http://localhost:3000"})
+@Slf4j
+@RestController
+@RequestMapping("/api/inst/archive")
+@RequiredArgsConstructor
+public class ArchiveController {
+
+    private final ArchiveService archiveService;
+
+
+    @GetMapping("/courseInfo/list/{cpg}")
+    public ResponseEntity<?> courseInfo(Authentication authentication, @PathVariable int cpg) {
+        // 로그인 된 강사 정보 추출
+        //UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        //String loginInst = userDetails.getUsername();
+        String loginInst = "김지훈";
+
+        CourseStdntInstListDTO courseInfoDTO = courseService.findOneLectListStdnt(cpg, loginInst);
+
+        return new ResponseEntity<>(courseInfoDTO, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/hlb/wizian_project/instructors/service/ArchiveService.java
@@ -0,0 +1,8 @@
+package com.hlb.wizian_project.instructors.service;
+
+import com.hlb.wizian_project.domain.*;
+
+public interface ArchiveService {
+
+
+}
